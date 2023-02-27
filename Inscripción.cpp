c7df010b22e26ca7060875ff4a07c0a4b1e1968b
René Sontay <125227307+ReneSontay@users.noreply.github.com>
2023-02-27 00:12:43 -0600

    Add files via upload

---
+++ Inscripción.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	string nom, ape;
+	int edad, carne;
+	int n1;
+	cout<<"- - - - Bienvenido al programa - - - -\n"<<endl;
+	cout<<"\nIngrese un nombre y un apellido: ";
+	cin>>nom;
+	cin>>ape;
+	cout<<"Ingrese el numero de carne que le fue asignado: ";
+	cin>>carne;
+	cout<<"Ingrese su edad: ";
+	cin>>edad;
+	cout<<"------------------------------------------------\n";
+	cout<<"\n1- Medicina"<<endl;
+	cout<<"2- Ingenieria"<<endl;
+	cout<<"3- Licenciatura"<<endl;
+	cout<<"4- Master"<<endl;
+	cout<<"Ingrese el numero de la carrera a seguir: ";
+	cin>>n1;
+	
+	/*aqui tiene que ir una condición multiple o anidada para saber
+	  que requisitos requiere cada carrera o facultad para luego
+	  y a la vez la condición de que si es mayor de edad presentar
+	  DPI propio y si no el de su encargado*/
+	
+	if(edad >= 18){
+		cout<<"\n- Presentar fotocopia de DPI"<<endl;
+		cout<<"- Presentar cierre de cursos"<<endl;
+		cout<<"- Asigancion de cursos"<<endl;
+	} else{
+		cout<<"\n- Presentar fotocopia de DPI del papa/mama o encargado"<<endl;
+		cout<<"- Presentar cierre de cursos"<<endl;
+		cout<<"- Asigancion de cursos"<<endl;
+		cout<<"-----------------------------------------------------------"<<endl;	
+	}
+	
+	return 0;
+}
